3e6d34c5b2e29225c216c3c0237dcd9d95abeb97
Ayat Ali <ayatali1661@gmail.com>
2023-06-21 18:43:50 +0300

    add some features for the video

--- video/urls.py
+++ video/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path ,include
 from . import views
 urlpatterns = [
-    path('',views.upload_video,name='upload_video'),
+    path('/upload_video',views.upload_video,name='upload_video'),
+    path('/videos',views.videos,name='videos'),
 ]
--- video/views.py
+++ video/views.py
@@ -2,16 +2,38 @@ from django.shortcuts import render, redirect
 from .form import VideoForm
 from django.shortcuts import get_object_or_404
 from .models import Users
+from django.contrib import messages
+from .models import Video
 
 def upload_video(request):
     if request.method == 'POST':
-        form = VideoForm(request.POST, request.FILES)
-        if form.is_valid():
-            video = form.save(commit=False)
-            user= get_object_or_404(Users,user=request.user)
-            video.uploaded_by = user
-            video.save()
-            return redirect('index')
+        user= get_object_or_404(Users,user=request.user)
+        if user.user_options == "Teacher":
+            form = VideoForm(request.POST, request.FILES)
+            if form.is_valid():
+                video = form.save(commit=False)
+                video.uploaded_by = user
+                video.save()
+                messages.success(request,'your data has been saved')
+            return redirect('upload_video')
+        else:
+            form = VideoForm()
+            messages.error(request,'You are not alowed to supmit video')
+            return render(request, 'pages/upload_video.html', {'form': form})
     else:
         form = VideoForm()
     return render(request, 'pages/upload_video.html', {'form': form})
+
+
+def videos(request):
+    if request.user is not None :
+        context=None
+        if not request.user.is_anonymous:
+            user= get_object_or_404(Users,user=request.user)
+            videos = Video.objects.filter(uploaded_by=user)
+            context={
+                'videos': videos
+            }
+        return render(request,'pages/videos.html',context)
+    else:
+        return redirect('videos')
