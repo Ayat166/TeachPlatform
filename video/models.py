f45b3d5b7ffc6e951d60891b75eea4ed153a8e64
Ayat166 <ayatali1661@gmail.com>
2023-07-21 11:50:14 +0300

    update templates

--- video/models.py
+++ video/models.py
@@ -3,10 +3,14 @@ from accounts.models import Users
 
 def upload_video_media(instance, filename):
     return "video/{0}/{1}".format(instance.uploaded_by.user.username, filename)
+def upload_image_media(instance, filename):
+    return "videoImage/{0}/{1}".format(instance.uploaded_by.user.username, filename)
+
 
 class Video(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+    image = models.ImageField("GroupImage",upload_to=upload_image_media, null=True, blank=True)
     file = models.FileField("video",upload_to=upload_video_media)
     uploaded_by = models.ForeignKey(Users, on_delete=models.CASCADE)
     uploaded_at = models.DateTimeField(auto_now_add=True)
